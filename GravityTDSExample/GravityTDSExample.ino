26f0d5469a66a3786f6a8f3c4998234e899cee4b
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-01-02 21:25:45 +0700

    Finish create example

--- GravityTDSExample/GravityTDSExample.ino
+++ GravityTDSExample/GravityTDSExample.ino
@@ -1,27 +1,3 @@
-/***************************************************
- DFRobot Gravity: Analog TDS Sensor/Meter
- <https://www.dfrobot.com/wiki/index.php/Gravity:_Analog_TDS_Sensor_/_Meter_For_Arduino_SKU:_SEN0244>
- 
- ***************************************************
- This sample code shows how to read the tds value and calibrate it with the standard buffer solution.
- 707ppm(1413us/cm)@25^c standard buffer solution is recommended.
- 
- Created 2018-1-3
- By Jason <jason.ling@dfrobot.com@dfrobot.com>
- 
- GNU Lesser General Public License.
- See <http://www.gnu.org/licenses/> for details.
- All above must be included in any redistribution.
- ****************************************************/
- 
- /***********Notice and Trouble shooting***************
- 1. This code is tested on Arduino Uno with Arduino IDE 1.0.5 r2 and 1.8.2.
- 2. Calibration CMD:
-     enter -> enter the calibration mode
-     cal:tds value -> calibrate with the known tds value(25^c). e.g.cal:707
-     exit -> save the parameters and exit the calibration mode
- ****************************************************/
-
 #include <EEPROM.h>
 #include "GravityTDS.h"
 
---
+++ README.md
@@ -0,0 +1,24 @@
+TDS probes cannot be used in water above 55 °C.
+The TDS probe should not be placed too close to the edge of the container, as it will affect the display.
+The head and lead of the TDS probe are waterproof and can be immersed in water, but the connection interface and the signal adapter board are not waterproof. Please pay attention to use.
+ 
+Product Features:
+ 
+1. Wide voltage operation: 3.3~5.5V
+2. 0~2.3V analog signal output, compatible with 5V, 3.3V two control systems
+3. The excitation source is an AC signal, effectively preventing probe polarization
+4. Waterproof probe for long-term immersion in water
+5. compatible, easy to connect, plug and play, no soldering required
+
+Technical specifications:
+ 
+Signal adapter board:
+ 
+Input voltage: 3.3~5.5V
+Output signal: 0~2.3V
+Working current: 3~6mA
+TDS measurement range: 0~1000ppm
+TDS measurement accuracy: ±10% F.S. (25°C)
+Size: 42*32mm
+Module interface: XH2.54-3P
+Electrode interface: XH2.54-2P 
